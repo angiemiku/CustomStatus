4372edde4802e0f7c7c542553b0dbbd37abea701
angiemiku <ilodecena53@gmail.com>
2025-02-22 14:16:30 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, CustomStatus } = require('discord.js-selfbot-v13');
+const client = new Client();
+const express = require('express');
+const app = express();
+
+////////////////////////////////////////////////////////////////
+const port = 3000 //Port
+const emoji = "🌙"; //Emoji here
+const state = "some custom status"; //Status
+const status = "dnd"; //online / idle / dnd / invisible
+const TOKEN = proccess.env.TOKEN || "DISCORD TOKEN HERE": //Your discord account TOKEN
+///////////////////////////////////////////////////////////////
+
+
+app.get('/', (req, res) => {
+  res.send('Success!!')
+});
+app.listen(port, () => {
+  console.log(`Listening to port: ${port}`);
+});
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+  const cs = new CustomStatus(client)
+    .setEmoji(emoji);
+    .setState(state);
+  client.user.setPresence({ activities: [cs], status: status });
+});
+
+client.login(TOKEN);
